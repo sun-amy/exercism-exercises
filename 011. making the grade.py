378840a7322d8a298b5176b16159c38eed0a43ea
Amy Sun <111322085+sun-amy@users.noreply.github.com>
2022-10-24 16:11:22 +0100

    Create 011. making the grade.py

---
+++ 011. making the grade.py
@@ -0,0 +1,81 @@
+"""
+Learn about loops by grading and organizing your students exam scores.
+"""
+
+"""Functions for organizing and calculating student exam scores."""
+
+
+def round_scores(student_scores):
+    """Round all provided student scores.
+
+    :param student_scores: list - float or int of student exam scores.
+    :return: list - student scores *rounded* to nearest integer value.
+    """
+    rounded_scores = []
+    for score in student_scores:
+        rounded_scores.append(round(score))
+    return rounded_scores
+
+def count_failed_students(student_scores):
+    """Count the number of failing students out of the group provided.
+
+    :param student_scores: list - containing int student scores.
+    :return: int - count of student scores at or below 40.
+    """
+    count = 0
+    for score in student_scores:
+        if score <= 40:
+            count += 1
+    return count
+
+def above_threshold(student_scores, threshold):
+    """Determine how many of the provided student scores were 'the best' based on the provided threshold.
+
+    :param student_scores: list - of integer scores.
+    :param threshold: int - threshold to cross to be the "best" score.
+    :return: list - of integer scores that are at or above the "best" threshold.
+    """
+    over_threshold = []
+    for score in student_scores:
+        if score >= threshold:
+            over_threshold.append(score)
+    return over_threshold
+
+def letter_grades(highest):
+    """Create a list of grade thresholds based on the provided highest grade.
+
+    :param highest: int - value of highest exam score.
+    :return: list - of lower threshold scores for each D-A letter grade interval.
+            For example, where the highest score is 100, and failing is <= 40,
+            The result would be [41, 56, 71, 86]:
+
+            41 <= "D" <= 55
+            56 <= "C" <= 70
+            71 <= "B" <= 85
+            86 <= "A" <= 100
+    """
+    difference = (highest - 40) // 4
+    return [41, 41 + difference, 41 + 2 * difference, 41 + 3 * difference]
+
+def student_ranking(student_scores, student_names):
+    """Organize the student's rank, name, and grade information in ascending order.
+
+    :param student_scores: list - of scores in descending order.
+    :param student_names: list - of string names by exam score in descending order.
+    :return: list - of strings in format ["<rank>. <student name>: <score>"].
+    """
+    student_and_score = []
+    for index, name in enumerate(student_names):
+        student_and_score.append(str(index + 1) + ". " + name + ": " + str(student_scores[index]))
+    return student_and_score
+
+def perfect_score(student_info):
+    """Create a list that contains the name and grade of the first student to make a perfect score on the exam.
+
+    :param student_info: list - of [<student name>, <score>] lists.
+    :return: list - first `[<student name>, 100]` or `[]` if no student score of 100 is found.
+    """
+    for student in student_info:
+        if student[1] == 100:
+            return student
+    return []
