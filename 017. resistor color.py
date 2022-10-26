6efcfc2c5183f36ff1b57c96bf13b0ea0ff34e38
Amy Sun <111322085+sun-amy@users.noreply.github.com>
2022-10-26 16:22:28 +0100

    Create 017. resistor color.py

---
+++ 017. resistor color.py
@@ -0,0 +1,10 @@
+"""
+Convert a resistor band's color to its numeric representation.
+"""
+
+def color_code(color):
+    colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+    return colors.index(color)
+
+def colors():
+    return ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
